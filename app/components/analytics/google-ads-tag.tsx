3250c69e2bf4ad6b47fb643fd1b6179314f70680
eidelman-products <eidelman.products@gmail.com>
2026-05-31 23:40:49 +0300

    Connect GA4 measurement id to Google tag

--- app/components/analytics/google-ads-tag.tsx
+++ app/components/analytics/google-ads-tag.tsx
@@ -3,6 +3,7 @@
 import Script from "next/script";
 
 const GOOGLE_ADS_ID = process.env.NEXT_PUBLIC_GOOGLE_ADS_ID;
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
 declare global {
   interface Window {
@@ -11,6 +12,20 @@ declare global {
   }
 }
 
+/**
+ * Central Google Tag component for Text2Task.
+ *
+ * Important:
+ * - The website already has a Google Ads tag installed.
+ * - GA4 was connected in Google Analytics as a destination of the same Google tag.
+ * - We load ONE Google tag script only, using the existing Google Ads ID.
+ * - We do NOT call gtag("config", GA_MEASUREMENT_ID) here, because that can create duplicate GA4 page views
+ *   when GA4 is already connected as a destination in Google tag settings.
+ *
+ * Env vars:
+ * - NEXT_PUBLIC_GOOGLE_ADS_ID=AW-XXXXXXXXXX
+ * - NEXT_PUBLIC_GA_MEASUREMENT_ID=G-XXXXXXXXXX
+ */
 export function GoogleAdsTag() {
   if (!GOOGLE_ADS_ID) {
     return null;
@@ -19,17 +34,26 @@ export function GoogleAdsTag() {
   return (
     <>
       <Script
+        id="google-tag-loader"
         src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_ID}`}
         strategy="afterInteractive"
       />
 
-      <Script id="google-ads-tag" strategy="afterInteractive">
+      <Script id="google-tag-init" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           window.gtag = window.gtag || gtag;
+
           gtag('js', new Date());
+
           gtag('config', '${GOOGLE_ADS_ID}');
+
+          ${
+            GA_MEASUREMENT_ID
+              ? `window.__TEXT2TASK_GA_MEASUREMENT_ID__ = '${GA_MEASUREMENT_ID}';`
+              : ""
+          }
         `}
       </Script>
     </>
